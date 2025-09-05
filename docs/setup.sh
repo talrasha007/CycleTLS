13a45ae51c3013bac10a548de2ab327a10261186
talrasha007 <talrasha007@gmail.om>
2025-09-05 19:03:09 +0800

    pure go proj.

--- docs/setup.sh
+++
@@ -1,3 +0,0 @@
-go env -w  GO111MODULE=auto
-golint *.go
-gofmt -w *.go
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
-
-global.performance = {
-  now: () => Date.now(),
-};
