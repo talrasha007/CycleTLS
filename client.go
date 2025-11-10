f0d5bd5a17ddd0a303b8d351ce96b3de54ab5e99
talrasha007 <talrasha007@gmail.om>
2025-11-10 15:28:16 +0800

    CloseIdleConnections when CleanupClientPool

--- client.go
+++ client.go
@@ -267,6 +267,9 @@ func CleanupClientPool(maxAge time.Duration) {
 	now := time.Now()
 	for key, entry := range advancedClientPool {
 		if now.Sub(entry.LastUsed) > maxAge {
+			if transport, ok := entry.Client.Transport.(*roundTripper); ok {
+				transport.CloseIdleConnections()
+			}
 			delete(advancedClientPool, key)
 		}
 	}
