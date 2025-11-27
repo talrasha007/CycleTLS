6b8cee5409863a2087a7d5b55be7cd7a2fef2dfd
Tal <talrasha007@gmail.com>
2025-11-27 13:43:33 +0800

    http code >=200

--- index.go
+++ index.go
@@ -1439,7 +1439,7 @@ func (client CycleTLS) Do(URL string, options Options, Method string) (Response,
 				// Use type assertion to access the roundTripper
 				transport.TotalRequests = 0
 				transport.CloseIdleConnections() // Close all idle connections
-			} else if enableConnectionReuse && resp.StatusCode < 400 {
+			} else if enableConnectionReuse && resp.StatusCode >= 200 && resp.StatusCode < 400 {
 				pushBackClientToPool(options.MaxIdleClients, httpClient, browser, options.Timeout, options.DisableRedirect, options.Meta, options.Proxy)
 			}
 		}
